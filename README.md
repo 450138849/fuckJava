174a4fbcc9843f771574c78e5c714c7646a33f27
bowen lv <450138849@qq.com>
2020-08-31 14:43:01 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # fuckJava
+
+## 项目说明
+
+- java基础
+- SSM
+- springboot
+- 项目开发
+
+- **设计模式提升**
+- **多线程提升+jvm+redis+zookeeper+mysql调优**
+- **亿级流量多级缓存**
+- **springcloud网约车**
+- MQ学习(RM)+netty+nio
---
+++ base/src/cn/fkJava/practice/Test415.java
@@ -0,0 +1,29 @@
+package cn.fkJava.practice;
+
+/**
+ * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和。
+ * 提示：
+ * num1 和num2 的长度都小于 5100
+ * num1 和num2 都只包含数字 0-9
+ * num1 和num2 都不包含任何前导零
+ * 你不能使用任何內建 BigInteger 库， 也不能直接将输入的字符串转换为整数形式
+ */
+public class Test415 {
+    public static void main(String[] args) {
+
+    }
+
+    public String addStrings(String num1, String num2) {
+        //1.两个数字从最低位开始相加,同时保存进位
+        //2.进位加进来后保存新的进位
+        //3.将计算的结果保存到新的字符串中
+        //4.字符串翻转
+        int i = 0;
+        int j = 0;
+        int carry = 0;
+
+
+        return "dasdsa";
+    }
+}
+
